d27329ff461443a2bccf71a9d100599cda0bed41
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-06-12 11:21:54 -0400

    Jun-12-2018_11:21_AM

---
+++ WorkingDemo2/WorkingDemo2/View/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkingDemo2.View;
+using Xamarin.Forms;
+
+namespace WorkingDemo2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+	}
+}
